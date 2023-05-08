4ca08f0dd221800a5a9723b6408385d220a200b2
Brian Smith <briansmith@rocketcentral.com>
2023-05-08 11:42:02 -0400

    Added localhost:8888 to list of allowed IP addresses

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ var filter = function (pathname, req) {
     req.headers.origin === "http://127.0.0.1:80" ||
     req.headers.origin === "https://127.0.0.1:80" ||
     req.headers.origin === "http://parks.punreliable.com" ||
-    req.headers.origin === "http://localhost:8888/parks" ||
-    req.headers.origin === "https://localhost:8888/parks"
+    req.headers.origin === "http://localhost:8888" ||
+    req.headers.origin === "https://localhost:8888"
   );
 };
 
